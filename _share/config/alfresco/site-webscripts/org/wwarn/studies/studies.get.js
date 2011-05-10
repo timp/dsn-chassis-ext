a1d75d379e4d476be52e75de81e4847704132b38
ian.wright@well.ox.ac.uk <ian.wright@well.ox.ac.uk@867f0267-a635-8440-a92b-4a75019b422d>
2011-05-10 14:52:50 +0000

    Revised studies dashlet

--- _share/config/alfresco/site-webscripts/org/wwarn/studies/studies.get.js
+++ _share/config/alfresco/site-webscripts/org/wwarn/studies/studies.get.js
@@ -1,15 +1,15 @@
-var conn = remote.connect("alfresco");
-// search cmis search api path
-var id = "/cmis/queries";
+const PREF_SITES = "org.alfresco.share.sites";
+const PREF_FAVOURITE_SITES = PREF_SITES + ".favourites";
+const PREF_IMAP_FAVOURITE_SITES = PREF_SITES + ".imapFavourites";
 
-  var selectString = "SELECT * FROM wc:study_folder";
-  var cmisQuery = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\" ?><query xmlns:cmis=\"http://docs.oasis-open.org/ns/cmis/core/200908/\"><statement>" + selectString + "</statement><pageSize>" + 0 + "</pageSize></query>";
-  
-  // get the documents for the currently active site
-  var conn = remote.connect("alfresco");
-  var queryResult = conn.post(stringUtils.urlEncodeComponent(id), cmisQuery, "application/cmisquery+xml");
-  // sitesdocuments[0] = "{name: " + sitename + ", documents: " + atom.toFeed(queryResult) + "}";
-//  var studies=atom.toFeed(queryResult);
+function main()
+{
+   var sites = [],
+      imapServerEnabled = false;
 
-model.results = atom.toFeed(queryResult);
-//model.studies=studies;
+   // Prepare the model for the template
+   model.sites = sites;
+   model.imapServerEnabled = imapServerEnabled;
+}
+
+main();
