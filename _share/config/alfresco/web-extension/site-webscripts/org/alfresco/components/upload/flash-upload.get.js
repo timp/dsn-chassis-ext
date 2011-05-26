d731e35762fdda899416cff8b54051517d127f09
gravitonian@gmail.com <gravitonian@gmail.com@867f0267-a635-8440-a92b-4a75019b422d>
2011-05-26 16:41:55 +0000

    Implemented Issue 8,9,13,14,15

--- _share/config/alfresco/web-extension/site-webscripts/org/alfresco/components/upload/flash-upload.get.js
+++ _share/config/alfresco/web-extension/site-webscripts/org/alfresco/components/upload/flash-upload.get.js
@@ -2,13 +2,13 @@ function getContentTypes()
 {
 	var contentTypes = [
 		{ id: "cm:content", value: "cm_content" },
-        { id: "wc:data_file", value: "type.wc_data_file" },
-        { id: "wc:data_dictionary", value: "type.wc_data_dictionary" },
+        { id: "wc:dataFile", value: "type.wc_dataFile" },
+        { id: "wc:dataDictionary", value: "type.wc_dataDictionary" },
         { id: "wc:protocol", value: "type.wc_protocol" },
         { id: "wc:publication", value: "type.wc_publication" },
         { id: "wc:other", value: "type.wc_other" },
         { id: "wc:output", value: "type.wc_output" },
-        { id: "wc:study_info", value: "type.wc_study_info" }
+        { id: "wc:studyInfo", value: "type.wc_studyInfo" }
 		];
 	return contentTypes;
 }
