b6bcf6fdf4f09a187982f3bfb6e5631152983323
gravitonian@gmail.com <gravitonian@gmail.com@867f0267-a635-8440-a92b-4a75019b422d>
2011-06-04 15:27:21 +0000

    Implemented the comment box for Issue 10

--- trunk/_alfresco/config/alfresco/extension/templates/webscripts/org/wwarn/derivedassoc/createDerivedAssoc.get.js
+++ trunk/_alfresco/config/alfresco/extension/templates/webscripts/org/wwarn/derivedassoc/createDerivedAssoc.get.js
@@ -2,10 +2,10 @@ var outputFileNodeRef = args.outputFileNodeRef;
 var studyFileNodeRef = args.studyFileNodeRef;
 
 if (outputFileNodeRef == null || studyFileNodeRef == null) {
-    model.message = "Could not setup association, outputFileNodeRef is null or studyFileNodeRef is null";
+    model.message = "Could not setup derived file association, outputFileNodeRef is null or studyFileNodeRef is null";
 } else {
     var outputFileNode = search.findNode(outputFileNodeRef);
     var studyFileNode = search.findNode(studyFileNodeRef);
-    outputFileNode.createAssociation(studyFileNode, "wc:derivations");
-    model.message = "Successfully setup association between outputFileNodeRef (" + outputFileNodeRef + ") and studyFileNodeRef (" + studyFileNodeRef + ")";
+    studyFileNode.createAssociation(outputFileNode, "wc:derivations");
+    model.message = "Successfully setup association between studyFileNodeRef (" + studyFileNodeRef + ") and outputFileNodeRef (" + outputFileNodeRef + ")";
 }
