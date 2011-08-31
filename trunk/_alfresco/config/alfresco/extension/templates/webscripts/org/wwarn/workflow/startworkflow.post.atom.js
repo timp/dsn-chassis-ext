c4939ffafe94f1b7f52cff9769bdcda555e6c277
ian.wright@well.ox.ac.uk <ian.wright@well.ox.ac.uk@867f0267-a635-8440-a92b-4a75019b422d>
2011-08-31 07:50:44 +0000

    Setup workflow via atom entry post

---
+++ trunk/_alfresco/config/alfresco/extension/templates/webscripts/org/wwarn/workflow/startworkflow.post.atom.js
@@ -0,0 +1,44 @@
+var postedObject = entry;
+var workflow = args['workflow'];
+
+function startWorkflow(workflowName, studyId, chassis) {
+var studyFolderPath = '/WWARN/Studies/' + studyId;
+
+var folder = null;
+if (studyFolderPath != null) {
+    folder = companyhome.childByNamePath(studyFolderPath);
+}
+model.folder = folder;
+if (workflowName == null || workflowName == "") {
+	status.code = 500;
+    status.message = "Could not start workflow, {workflowName} has not been specified";
+} else if (studyId == null || studyId == "") {
+	status.code = 500;
+    status.message = "Could not start workflow, {studyId} has not been specified";
+} else {
+
+    var workflow = actions.create("start-workflow");
+    workflow.parameters.workflowName = "jbpm$" + workflowName;
+    //workflow.parameters["bpm:workflowDescription"] = upload.name;
+    workflow.parameters["ww:studyID"] = studyId;
+    workflow.parameters["ww:studyFolderLink"] = '/share/page/folder-details?nodeRef=' + folder.nodeRef;
+    workflow.parameters["wc:studyInfoLink"] = '/repository/study/dashboard?tab=study-info&study=' + chassis;
+    //workflow.parameters["ww:contributorEmail"] = contributorEmail;
+    //workflow.parameters["wc:modules"] = modules;
+    //workflow.parameters["wc:region"] = region;
+    //workflow.parameters["wc:countries"] = countries;
+    workflow.execute(folder);
+
+    status.code = 201;
+    status.message = "Started workflow " + workflowName + " for study " + studyId;
+}
+}
+var studyId = postedObject.title;
+var chassis = postedObject.id;
+
+startWorkflow(workflow, studyId, chassis);
+model.message = status.message;
+model.workflow = workflow;
+model.study = studyId;
+model.chassis = chassis;
+logger.log("Message: " + status.message);
